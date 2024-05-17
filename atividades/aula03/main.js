9bbd03c14f3ce71911f7789c35600d6281489871
aluno <aluno@LAB-24.ic.ufba.br>
2024-05-17 15:00:15 -0300

    imc funcional

--- atividades/aula03/main.js
+++ atividades/aula03/main.js
@@ -1,21 +1,21 @@
 // crie aqui as variáveis dos objetos ue irá manipular
+let peso = 0;
+let altura = 0;
 
 // funcao de calculo
-function calcularIMC() {
+function calcularIMC(peso, altura) {
   // crie as variáveis peso e altura, obtendo os dados do html
-  const peso = parseFloat(document.getElementById("peso").value);
-  const altura = parseFloat(document.getElementById("altura").value);
+  peso = parseFloat(document.getElementById("peso").value);
+  altura = parseFloat(document.getElementById("altura").value);
 
   // será necessário converter as variáveis?
 
   // calcule o IMC
-  const imc = peso((altura / 100) ** 2);
+  let imc = peso/((altura / 100) ** 2);
 
   // Atribua o resultado do imc à variável do HTML
   document.getElementById("result").value = imc.toFixed(2);
 }
 
 // adicione o evento listener
-document
-  .getElementById("calcularIMCButton")
-  .addEventListener("click", calcularIMC);
+document.getElementById("calcularIMCButton").addEventListener("click", calcularIMC);
