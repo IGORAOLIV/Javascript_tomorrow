1bd113554b0b03116f8c939261aa3776bb91586d
aluno <aluno@LAB-24.ic.ufba.br>
2024-05-17 16:13:18 -0300

    correção professor

--- atividades/aula03/calculadora_imc/main.js
+++ atividades/aula03/calculadora_imc/main.js
@@ -1,21 +1,37 @@
 // crie aqui as variáveis dos objetos ue irá manipular
-let peso = 0;
-let altura = 0;
+const buttonCalIMC = document.getElementById("calcularIMCButton");
+const resultIMC = document.getElementById("result");
+const faixasIMC = document.querySelectorAll('.faixa');
 
 // funcao de calculo
-function calcularIMC(peso, altura) {
+function calcularIMC() {
   // crie as variáveis peso e altura, obtendo os dados do html
-  peso = parseFloat(document.getElementById("peso").value);
-  altura = parseFloat(document.getElementById("altura").value);
-
-  // será necessário converter as variáveis?
+  let nPeso = parseFloat(document.getElementById("peso").value);
+  let nAltura = parseFloat(document.getElementById("altura").value);
 
   // calcule o IMC
-  let imc = peso/((altura / 100) ** 2);
+  let imc = nPeso/((nAltura / 100) ** 2);
 
   // Atribua o resultado do imc à variável do HTML
-  document.getElementById("result").value = imc.toFixed(2);
+  resultIMC.value = imc.toFixed(2);
+
+  if (imc < 18.5) {
+    document.querySelector('.level1').style.background = "#FF5733"; 
+  } else if (imc >= 18.5 && imc < 25) {
+    document.querySelector('.level2').style.background = "#4caf50";
+  } else if (imc >= 25 && imc < 30){
+    document.querySelector('.level3').style.background = "#fce311";
+  } else if (imc >= 30 && imc < 35){
+    document.querySelector('.level4').style.background = "#FF5733";
+  } else if (imc >= 35 && imc < 40){
+    document.querySelector('.level5').style.background = "#FF5733";
+  } else {
+    document.querySelector('.level6').style.background = "#FF5733";
+  }
 }
 
 // adicione o evento listener
-document.getElementById("calcularIMCButton").addEventListener("click", calcularIMC);
+calcularIMCButton.addEventListener("click", calcularIMC);
+
+
+
